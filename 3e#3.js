135d2ecd75182158f5f2d28a084d630d1b195cb1
yanush3322 <33556030+yanush3322@users.noreply.github.com>
2018-10-03 09:00:57 +0200

    Add files via upload

---
+++ 3e#3.js
@@ -0,0 +1,34 @@
+<html>
+<head>
+</head>
+<body>
+
+<script>
+
+//zadanie 1
+
+const text="kobyla ma maly bok";
+console.log(text.toUpperCase()); 
+console.log(text.length); 
+
+//zadnanie 2 
+
+var x=65021113122	
+var =x.toString();
+var d=y.length;	
+alert (y[d-2]0;
+
+if ( y[[d-2] & 2 ==0)
+alert ("kobieta")
+else
+alert ("kobieta")
+
+
+
+
+
+
+</script>
+
+</body>
+</html>
